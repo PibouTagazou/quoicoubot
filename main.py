d4265d43f22a392407efc180c0d8cacb2f6cfdb6
PibouTagazou <136444473+PibouTagazou@users.noreply.github.com>
2023-06-13 11:48:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+from discord import Client, Intents
+from random import choice
+import os
+import string
+import metadata
+
+intents = Intents.default()
+intents.message_content = True
+client = Client(intents=intents)
+
+feur_like = {
+  "FEUR": "feur",
+  "FEUR2": "FEUR !!!!!",
+  "FEUR3": "quoicoubeh",
+  "FEUR4": "feur je pense",
+  "FEUR5": "COUBEEEEEEEEEEEEEEEEEEEH",
+  "FEUR6": "et c'est le feur !",
+  "FEUR7": "react"
+}
+
+
+@client.event
+async def on_ready():
+  print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+  message_test = message.content.lower().replace(" ", "").translate(
+    str.maketrans('', '', string.punctuation))
+  if message.author == client.user:
+    return
+
+  if (message_test.endswith('quoi') or message_test.endswith('quoient')
+      or message_test.endswith('kwa') or message_test.endswith('kwoi')
+      or message_test.endswith('kouaent') or message_test.endswith('qoua')
+      or message_test.endswith('koua') or message_test.endswith('qwa')
+      or message_test.endswith('koi')):
+    feur = choice(list(feur_like.items()))
+    if feur[1] != "react":
+      await message.channel.send(f'{feur[1]}')
+    else:
+      await message.add_reaction('🇫')
+      await message.add_reaction('🇪')
+      await message.add_reaction('🇺')
+      await message.add_reaction('🇷')
+
+  if message_test.endswith('waf') or message_test.endswith('ouaf'):
+    await message.channel.send('*pat pat*')
+
+  if message.content == "!horny":
+    await message.channel.send(choice(metadata.horny))
+
+
+if __name__ == "__main__":
+
+  my_secret = os.environ['quoicoubot_id']
+  client.run(my_secret)
---
+++ metadata.py
@@ -0,0 +1,10 @@
+horny = [
+  "good girl :3", "Alors comme ca on aime bien se faire attacher ? :smirk:",
+  "*te grab par les hanches*", "Je vais devoir régler ca on dirait :smirk:",
+  "Aboies pour moi.", "T'es tellement bottom :face_with_hand_over_mouth: ",
+  "Tu sais que t'es mignonne quand t'es horny comme ca :3",
+  "Tu me donnes envie de te faire des choses >:3", "Toujours horny celle la",
+  "Tu sais qu'une laisse t'irait si bien...",
+  "Ca serait dommage de laisser une petite bottom comme toi toute horny et désespérée :eyes:",
+  "Ta bouche est inutilement vide, vient te mettre au travail ça te détendra."
+]
